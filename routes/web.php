51e80e1092fa437ddc93eee60888207674a9604d
unknown <hosseinbidar7@gmail.com>
2020-09-26 16:14:11 +0330

    0.2

--- routes/web.php
+++ routes/web.php
@@ -18,54 +18,54 @@ use Illuminate\Support\Facades\Route;
 //AUTH USERS ROUTES
 
 
-//******************************//
-Route::get('/','Controller@index');
-Route::get('dashboard','Controller@index')->name('dashboard');
-Route::get('delete/{id}','Controller@delete');
+    //******************************//
+    Route::get('/', 'Controller@index');
+    Route::get('dashboard', 'Controller@index')->name('dashboard');
+    Route::get('delete/{id}', 'Controller@delete');
 
-                                    //SIGNUP//
-Route::get('register','userLoginController@register')->name('register_user_page');
-Route::post('register','userLoginController@registerform')->name('register_user');
-                                    //SIGNUP//
+    //SIGNUP//
+    Route::get('register', 'userLoginController@register')->name('register_user_page');
+    Route::post('register', 'userLoginController@registerform')->name('register_user');
+    //SIGNUP//
 
-                                    //LOGIN//
-Route::get('auth','userLoginController@auth_user')->name('auth_user');
-Route::post('auth','userLoginController@doLogin')->name('login_user');
-                                   //LOGIN//
+    //LOGIN//
+    Route::get('auth', 'userLoginController@auth_user')->name('auth_user');
+    Route::post('auth', 'userLoginController@doLogin')->name('login_user');
+    //LOGIN//
 
-                                   //FORGET PASSWORD//
-Route::get('forgetpassword','userLoginController@forgetpassword')->name('forgetpassword');
-Route::post('forgetpassword','userLoginController@doforgetpassword');
-                                   //FORGET PASSWORD//
+    //FORGET PASSWORD//
+    Route::get('forgetpassword', 'userLoginController@forgetpassword')->name('forgetpassword');
+    Route::post('forgetpassword', 'userLoginController@doforgetpassword');
+    //FORGET PASSWORD//
 
-                                   //LOGOUT//
-Route::get('logoutuser','userLoginController@logout')->name('logout_user');
-                                   //LOGOUT//
+    //LOGOUT//
+    Route::get('logoutuser', 'userLoginController@logout')->name('logout_user');
+    //LOGOUT//
 
-                                   //SOCHIAL LOGIN//
-Route::get('auth/google', 'userLoginController@redirectToProvider')->name('googleLogin');
-Route::get('auth/google/callback', 'userLoginController@handleProviderCallback');
-                                   //SOCHIAL LOGIN//
-//******************************//
-
-
-//AUTH USERS ROUTES
+    //SOCHIAL LOGIN//
+    Route::get('auth/google', 'userLoginController@redirectToProvider')->name('googleLogin');
+    Route::get('auth/google/callback', 'userLoginController@handleProviderCallback');
+    //SOCHIAL LOGIN//
+    //******************************//
 
 
-//EXTRA PAGES
-//******************************//
-Route::get('posts', 'userPostsController@index')->name('posts');
-Route::get('postscreate', 'userPostsController@create')->name('postscreate');
-Route::post('postscreate', 'userPostsController@store');
-Route::get('postsedit', 'userPostsController@edit')->name('postsedit');
-Route::get('postsedit{id}', 'userPostsController@postedit')->name('postedit');
-Route::post('postsedit{id}', 'userPostsController@update')->name('update');
-Route::get('postsdelete{id}', 'userPostsController@destroy')->name('postsdelete');
-Route::get('postlist', 'userPostsController@postlist')->name('postslist');
-//******************************//
-//EXTRA PAGES
-//category
-Route::get('categorycreate', 'Category@index')->name('categorycreate');
-Route::post('categorycreate', 'Category@store');
-//category
+    //AUTH USERS ROUTES
+
+
+    //EXTRA PAGES
+    //******************************//
+    Route::get('posts', 'userPostsController@index')->name('posts');
+    Route::get('postscreate', 'userPostsController@create')->name('postscreate');
+    Route::post('postscreate', 'userPostsController@store');
+    Route::get('postsedit', 'userPostsController@edit')->name('postsedit');
+    Route::get('postsedit{id}', 'userPostsController@postedit')->name('postedit');
+    Route::post('postsedit{id}', 'userPostsController@update')->name('update');
+    Route::get('postsdelete{id}', 'userPostsController@destroy')->name('postsdelete');
+    Route::get('postlist', 'userPostsController@postlist')->name('postslist');
+    //******************************//
+    //EXTRA PAGES
+    //category
+    Route::get('categorycreate', 'Category@index')->name('categorycreate');
+    Route::post('categorycreate', 'Category@store');
+    //category
 
